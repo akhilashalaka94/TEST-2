d063ac6cd020c1e7f82eca16de28eaa779df7493
Akhila Shalaka <97923680+akhilashalaka94@users.noreply.github.com>
2022-02-09 20:28:37 +1100

    Create button.component.jsx

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./components/button/button.styles.scss";
+
+const CustomButton = () => {
+    return (
+        <button onClick => {alert('The Button has been clicked!')}>
+            Click me!
+        </button>
+    );
+};
+
+export default CustomButton;
